d853848d970cf58f1a98715dac309f6a51d5abdd
jojonki <junki.ohmura@gmail.com>
2022-12-29 15:34:47 +0900

    support manifest v3

--- background.js
+++ background.js
@@ -11,37 +11,54 @@ function copyToClipboard(text) {
     document.body.removeChild(input);
 };
 
-function copyTextWithURL(tab, copy_mode) {
-    console.log("called copyTextWithURL");
-    let url = tab.url;
-    chrome.tabs.executeScript( {
-        code: "window.getSelection().toString();"
-    }, function(selection) {
-        console.log("selection", selection);
-        if (selection.length > 0) {
-            let selected_text = selection[0];
-            console.log("selected_text", selected_text);
-            if (copy_mode === "plain") {
-                copyToClipboard(selected_text + "\n" + url);
-            } else if (copy_mode === "markdown") {
-                copyToClipboard("[" + selected_text + "](" + url + ")");
-            }
-            let options = {
-                type: "basic",
-                iconUrl: "./images/icon_128.png",
-                title: "Copied as " + copy_mode + "!",
-                message: "\"" + selected_text + "\"",
-            }
-            console.log("call notifications");
-            chrome.notifications.create('copy_with_text_notification', options);
-        } else {
-            alert("TODO no selected text");
-        }
-    });
+function getSelection() {
+    return window.getSelection().toString();
 }
 
-chrome.commands.onCommand.addListener(function(command) {
-    console.log("onCommand:", command);
+function copyTextWithURL(tab, copy_mode) {
+    // console.log("called copyTextWithURL");
+    let url = tab.url;
+    let tabId = tab.id;
+    chrome.scripting.executeScript(
+        {
+            target: { tabId: tabId },
+            func: getSelection
+        },
+        (res) => {
+            let selection = res[0].result;
+            if (selection.length > 0) {
+                console.log("selected_text", selection);
+                let text = "---"
+                if (copy_mode === "plain") {
+                    text = selection + "\n" + url;
+                } else if (copy_mode === "markdown") {
+                    text = "[" + selection + "](" + url + ")";
+                }
+                chrome.scripting.executeScript(
+                    {
+                        target: { tabId: tabId },
+                        func: copyToClipboard,
+                        args: [text],
+                    },
+                    (res) => {
+                        let options = {
+                            type: "basic",
+                            iconUrl: "./images/icon_128.png",
+                            title: "Copied as " + copy_mode + " text.",
+                            message: "\"" + selection + "\"",
+                        }
+                        chrome.notifications.create(`copy_with_text_notification-${Date.now()}`, options);
+
+                    });
+            } else {
+                alert("TODO no selected text");
+            }
+
+        });
+}
+
+chrome.commands.onCommand.addListener(function (command) {
+    // console.log("onCommand:", command);
     let copy_mode = null;
     if (command === "copy-text-with-url-as-plain") {
         copy_mode = "plain"
@@ -51,17 +68,16 @@ chrome.commands.onCommand.addListener(function(command) {
         alert("Unknown command: " + command);
     }
 
-    console.log("copy mode", copy_mode);
+    // console.log("copy mode", copy_mode);
     if (copy_mode !== undefined) {
-        console.log("call tabs.query to activeTab");
+        // console.log("call tabs.query to activeTab");
         chrome.tabs.query({
             active: true,
             lastFocusedWindow: true
-        }, function(tabs) {
+        }, function (tabs) {
             console.log("call copyTextWithURL");
             copyTextWithURL(tabs[0], copy_mode);
         });
     }
 });
 
-chrome.browserAction.onClicked.addListener(copyTextWithURL);
